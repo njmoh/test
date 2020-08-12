ecb83438ecb214c1086028a41653f88d9fc1e374
njmoh <50534445+njmoh@users.noreply.github.com>
2020-08-12 22:55:02 +0300

    Create number.java

---
+++ number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class num{
+	public static void main(string[]args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("---- This program check if the number is even or odd ----")
+
+
+
+
+	}
+}
